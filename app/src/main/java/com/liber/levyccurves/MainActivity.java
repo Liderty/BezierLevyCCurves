09ea50143b1aeec053165b58c0dc4ecd15f925ef
Liderty <mateusz.liber@smcebi.edu.pl>
2020-11-25 14:00:44 +0100

    Initial Commit

---
+++ app/src/main/java/com/liber/levyccurves/MainActivity.java
@@ -0,0 +1,14 @@
+package com.liber.levyccurves;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
