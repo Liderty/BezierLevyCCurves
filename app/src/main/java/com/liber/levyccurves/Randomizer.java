8d60407ace423eb1850922f5dfcde7383d8f94eb
Liderty <liderty@o2.pl>
2020-12-15 21:28:46 +0100

    Update color randomizer

--- app/src/main/java/com/liber/levyccurves/Randomizer.java
+++ app/src/main/java/com/liber/levyccurves/Randomizer.java
@@ -1,30 +1,22 @@
 package com.liber.levyccurves;
 
 public class Randomizer {
-    private static String[] mColors = {
-            "#39add1", // light blue
-            "#3079ab", // dark blue
-            "#c25975", // mauve
-            "#e15258", // red
-            "#f9845b", // orange
-            "#838cc7", // lavender
-            "#7d669e", // purple
-            "#53bbb4", // aqua
-            "#51b46d", // green
-            "#e0ab18", // mustard
-            "#637a91", // dark gray
-            "#f092b0", // pink
-            "#b7c0c7"  // light gray
-    };
+    static private int COLOR_MAX = 255;
 
     public static int generate(int min, int max) {
         return min + (int)(Math.random() * ((max - min) + 1));
     }
 
     public static String getRandomColor() {
-        String color = "";
-        int randomNumber = generate(0, mColors.length-1);
-        color = mColors[randomNumber];
-        return color;
+        String color = "#";
+
+        String red = Integer.toHexString(generate(0, COLOR_MAX));
+        if(red.length() == 1) red = "0" + red;
+        String green = Integer.toHexString(generate(0, COLOR_MAX));
+        if(green.length() == 1) green = "0" + green;
+        String blue = Integer.toHexString(generate(0, COLOR_MAX));
+        if(blue.length() == 1) blue = "0" + blue;
+
+        return color + red + green + blue;
     }
 }
